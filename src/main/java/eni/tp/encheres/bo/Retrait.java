ea08116c97776550806c3b0e3dc2cda285f71079
éthanlebris <ethan.lebris2024@campus-eni.fr>
2025-01-27 14:26:51 +0100

    1

--- src/main/java/eni/tp/encheres/bo/Retrait.java
+++ src/main/java/eni/tp/encheres/bo/Retrait.java
@@ -1,5 +1,40 @@
 package eni.tp.encheres.bo;
 
 public class Retrait {
+    private String rue;
+    private long code_postal;
+    private String ville;
+
+    public Retrait(String rue, long code_postal, String ville) {
+        super();
+        this.rue = rue;
+        this.code_postal = code_postal;
+        this.ville = ville;
+
+    }
+
+    public String getRue() {
+        return rue;
+    }
+
+    public void setRue(String rue) {
+        this.rue = rue;
+    }
+
+    public long getCode_postal() {
+        return code_postal;
+    }
+
+    public void setCode_postal(long code_postal) {
+        this.code_postal = code_postal;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
 
 }
