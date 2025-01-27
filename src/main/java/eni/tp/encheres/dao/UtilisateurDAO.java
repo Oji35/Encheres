2f17b40b862e90987535cca362948809598e5839
jnagel2024 <jonathan_591@hotmail.fr>
2025-01-27 15:42:53 +0100

    Merge remote-tracking branch 'origin/main'

--- src/main/java/eni/tp/encheres/dao/UtilisateurDAO.java
+++ src/main/java/eni/tp/encheres/dao/UtilisateurDAO.java
@@ -1,6 +1,22 @@
 package eni.tp.encheres.dao;
 
+import eni.tp.encheres.bo.Utilisateur;
+
+import java.util.List;
+
 public interface UtilisateurDAO {
+
+    int createUtilisateur(Utilisateur utilisateur);
+
+    Utilisateur readUtilisateur(int id);
+
+    List<Utilisateur> readAllUtilisateurs();
+
+    void updateUtilisateur(Utilisateur utilisateur);
+
+    void deleteUtilisateur(int id);
+
+    void deleteUtilisateur(Utilisateur utilisateur);
 
 
 
