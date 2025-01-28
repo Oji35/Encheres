fe97cb415c090980249276f802915a3a38ae9c83
SedLexLam <alexandre.lelamer@gmail.com>
2025-01-28 11:01:05 +0100

    Merge remote-tracking branch 'origin/main'

--- src/main/java/eni/tp/encheres/dao/UtilisateurDAO.java
+++ src/main/java/eni/tp/encheres/dao/UtilisateurDAO.java
@@ -1,6 +1,22 @@
 package eni.tp.encheres.dao;
 
+import eni.tp.encheres.bo.Utilisateur;
+
+import java.util.List;
+
 public interface UtilisateurDAO {
+
+    int createUtilisateur(Utilisateur utilisateur);
+
+    Utilisateur readUtilisateur(int id);
+
+    List<Utilisateur> readAllUtilisateurs();
+
+    void CallIDForDelete(Utilisateur utilisateur);
+
+    void updateUtilisateur(Utilisateur utilisateur);
+
+    void deleteUtilisateur(long id);
 
 
 
