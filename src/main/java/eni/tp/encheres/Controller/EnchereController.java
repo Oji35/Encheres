77f677bfcd7c7c4b17d4e43bea222cd08c378137
éthanlebris <ethan.lebris2024@campus-eni.fr>
2025-01-30 09:31:48 +0100

    1

--- src/main/java/eni/tp/encheres/Controller/EnchereController.java
+++ src/main/java/eni/tp/encheres/Controller/EnchereController.java
@@ -1,12 +1,15 @@
 package eni.tp.encheres.Controller;
 
 import eni.tp.encheres.bll.ArticleService;
+import eni.tp.encheres.bo.ArticleVendu;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping
@@ -34,9 +37,9 @@ public class EnchereController {
 
     @GetMapping("/view-encheres")
     public String afficherListeEnchere(Model model) {
-        var article = articleService.getAllArticles();
-        model.addAttribute("article", article);
-        return "view-encheres";
+        List<ArticleVendu> articles = articleService.getAllArticles(); // Vérifie que cette méthode fonctionne
+        model.addAttribute("articles", articles); // Doit être au pluriel comme dans Thymeleaf
+        return "view-encheres"; // Assure-toi que view-encheres.html existe
     }
 
     @PostMapping("/view-encheres")
