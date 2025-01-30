4fef5f6a41ee0f5045511383d9509c5d22125774
éthanlebris <ethan.lebris2024@campus-eni.fr>
2025-01-30 16:50:27 +0100

    1

---
+++ src/main/java/eni/tp/encheres/bll/ArticleService.java
@@ -0,0 +1,17 @@
+package eni.tp.encheres.bll;
+
+import eni.tp.encheres.bo.ArticleVendu;
+
+import java.util.List;
+
+public interface ArticleService {
+    void addArticleVendu(ArticleVendu article);
+
+    void removeArticleVenduParId(int id);
+
+    ArticleVendu getArticleVendubyID(int id);
+
+    void update(ArticleVendu article);
+
+    List<ArticleVendu> getAllArticles();
+}
